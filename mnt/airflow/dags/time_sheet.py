f9ff3a5c7967a1cb17e88105ba9b564115d50146
Gautami Mamidala <gautamisaisatya@gmail.com>
2024-02-09 13:55:13 +0000

    trying to download file

--- mnt/airflow/dags/time_sheet.py
+++ mnt/airflow/dags/time_sheet.py
@@ -1,7 +1,19 @@
 from airflow import DAG
-from airflow.operators.dummy import DummyOperator
-from datetime import datetime, timedelta
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.operators.dummy import DummyOperator
+from airflow.models.connection import Connection
+from airflow.decorators import task
+from datetime import datetime, timedelta
+import urllib.request
+
+c = Connection(
+    conn_id = "http_sensor",
+    conn_type="http",
+    host="https://docs.google.com/spreadsheets/d/e/",
+)
+
+LINK_TO_DOWNLOAD = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ59Q-NafCjOPNsIXLUqaPazXbZJvf9aPdD8W2CBpVAoSP-Eb0F92efvd2znIHFwRW7KelDcdx4ts9M/pub?gid=1304433456&single=true&output=csv"
+ENDPOINT = "2PACX-1vQ59Q-NafCjOPNsIXLUqaPazXbZJvf9aPdD8W2CBpVAoSP-Eb0F92efvd2znIHFwRW7KelDcdx4ts9M/pub?gid=1304433456&single=true&output=csv"
 
 default_args = {
     "owner": "airflow",
@@ -12,6 +24,12 @@ default_args = {
     "retry_delay": timedelta(minutes=5)
 }
 
+@task
+def download_file(task_id, uri, target_path):
+    with urllib.request.urlopen(uri) as file:
+        with open(target_path, "wt") as new_file:
+            new_file.write(file.read())
+
 with DAG("weekly_time_sheet", 
             start_date = datetime(2024,1,1), 
             schedule_interval="@daily",
@@ -19,13 +37,24 @@ with DAG("weekly_time_sheet",
             catchup = False,
             ) as dag:
 
+
         start_process = DummyOperator(
             task_id = 'start_process',
 
         )
 
-        get_spreadsheet_data = DummyOperator(
+        put_http_sensor = HttpSensor(
+            task_id = 'put_http_sensor',
+            http_conn_id = 'http_sensor',
+            endpoint = ENDPOINT ,
+            poke_interval=5,
+            timeout=20
+        )
+
+        get_spreadsheet_data = download_file(
             task_id = 'get_spreadsheet_data',
+            uri=LINK_TO_DOWNLOAD, 
+            target_path='/opt'
         )
 
         load_data_to_postgres = DummyOperator(
@@ -56,7 +85,7 @@ with DAG("weekly_time_sheet",
             task_id = 'end_process',
         )
 
-        start_process >> get_spreadsheet_data >> load_data_to_postgres 
+        start_process >> put_http_sensor >> get_spreadsheet_data >> load_data_to_postgres 
         load_data_to_postgres >> sum_by_day >> group_by_date_and_person 
         group_by_date_and_person >> filter_by_week >> create_weekly_report
         create_weekly_report >> send_an_email >> end_process
